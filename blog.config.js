124cd403a8772879f401769bf47c0144cc8dcaa4
Jackie Wu <junbujian6216@gmail.com>
2025-01-31 03:39:24 +0800

    update heo config

--- blog.config.js
+++ blog.config.js
@@ -17,10 +17,8 @@ const BLOG = {
     AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'JackieWu', // 您的昵称 例如 tangly1024
     BIO: process.env.NEXT_PUBLIC_BIO || 'Just the way you are', // 作者简介
     LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.jackiewu.top', // 网站地址
-    CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || 'https://twitter.com/JackieWuu9527', // 你的twitter个人主页
     KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'NAS, unRAD, Docker, VM, Homelab', // 网站关键词 英文逗号隔开
     BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
-
     BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
     BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
 
@@ -47,14 +45,15 @@ const BLOG = {
     ...require('./conf/notion.config'), // 读取notion数据库相关的扩展配置，例如自定义表头
     ...require('./conf/dev.config'), // 开发、调试时需要关注的配置
 
-    // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
-    LAYOUT_SIDEBAR_REVERSE: false,
+    // 自定义外部脚本，外部样式
+    CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
+    CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
     // 自定义菜单
-    CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
+    CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
 
     // 文章列表相关设置
-    CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || false, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
+    CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
 
     // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
     LAYOUT_SIDEBAR_REVERSE:
